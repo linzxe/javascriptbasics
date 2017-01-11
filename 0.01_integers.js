6fd89506cb9ced909b6c36b4bb5dfa7ebeb2fd52
John Mergy <johnmergy@Johns-MacBook-Air.local>
2017-01-11 11:13:04 -0500

    first js added integers and variables

---
+++ 0.01_integers.js
@@ -0,0 +1,40 @@
+//--------------------------------------------------------
+//--------------------------------------------------------
+//------------- Challenge: Integers--------------
+//--------------------------------------------------------
+//--------------------------------------------------------
+
+/*CHALLENGE: Bronze 
+Create four integers: your age, birth year, graduation year, and number of brothers and sisters. 
+Print them to the console. Put comments above each one to identify.
+*/
+
+/* CHALLENGE: Silver
+Using the modulus, write some simple modulus equations that will yield the numbers below. Use only two numbers:
+1, 2, 12
+*/
+
+
+/* CHALLENGE: Gold
+Research how to create an exponent. Create one.
+*/
+
+//my age is 18//
+console.log(18);
+//my birthyear is 1997//
+console.log(1997);
+//i graduated in 2016//
+console.log(2016);
+//i have three sisters//
+console.log(3);
+
+//silver challenge//
+console.log(11%2);
+console.log(11%3);
+console.log(38%13);
+
+//gold challenge//
+console.log(Math.pow(12, 2));
+//can also use below to get 2 to the third power//
+console.log(2e3);
+
---
+++ 0.02_variables.js
@@ -0,0 +1,63 @@
+/*       var firstname = "lindz"
+var lastname = "mergy"
+var fullname = firstname + " " + lastname
+//concatenation...building and combining strings//
+console.log(fullname)
+//add " " to add a space automatically//
+
+//empty variables//
+var num
+console.log(num)
+//doesnt work bc it has no value assigned to it....it has been called, but not declared//
+//js uses context more than other languages//
+var petsName = ""
+//have to declare an empty string for user to fill in the field(even though we dont have that information yet), so i can call that data later 
+//program for the next person to read your code easily,  understandably, and quickly//
+//global work atmosphere.....no var within var within var//
+//can use different naming conventions for local blocks of code so they are local to where they are//
+//let allows local use only, is more secure. vs var....makes global use easier.
+let
+if(true){
+	let petsName =" " */
+
+/*console.log(petsName)      
+
+// short hand operators //
+var age = 55
+
+/* console.log(age += 1)//1 =1+1.....this is a false statement//
+console.log(age +=1)//this is saying age = age + 1// */
+//console.log(age /= 2)//
+//console.log(age *= 2)//
+//var 2nite ="Wednesday Night"......DO NOT USE INTEGERS IN YOUR VARIABLES//
+
+
+
+/*
+var howManyHotdogsIate = 1500;
+var howManyTurkeysIate = 500;
+
+console.log(howManyHotdogsIate + howManyTurkeysIate); */
+
+/*
+var myNumber = 0
+console.log(myNumber +=10);
+console.log(myNumber -=1);
+console.log(myNumber *=9);
+console.log(myNumber /=7);
+
+*/
+
+var currentAge = 19;
+var maxAge = 119;
+var avgWater = 5;
+var avgLifetimeWater = ((maxAge - currentAge) * avgWater)
+console.log("You will need " + avgLifetimeWater + " glasses of water to last you from now until the ripe old age of 116");
+//can use single or double quotes for adding in the string//
+//can put the whole printed message in another variable to clean it up//
+
+
+
+
+
+
